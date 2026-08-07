69bd8fbff87ce08d337d3022468476ad4f5b5006
Zanottex <gustavozanotto119@gmail.com>
2026-08-07 19:15:27 -0300

    DAW

---
+++ DAW-2025-main/the-moviebox-react/src/App.tsx
@@ -0,0 +1,20 @@
+import { MovieContent } from "./cases/movies/components/movie-content"
+import { Footer } from "./components/layout/footer"
+import { Header } from "./components/layout/header"
+import { Navbar } from "./components/layout/navbar"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main>
+        <Navbar />
+        <MovieContent />
+      </main>  
+      <Footer />
+    </>
+  )
+}
+
+export default App
